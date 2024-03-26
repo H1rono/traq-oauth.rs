0f9f50c417b006978d14af217075806981d97cf8
H1rono <hronok66@gmail.com>
2024-03-27 01:39:15 +0900

    Sendable authorized code

--- src/routes.rs
+++ src/routes.rs
@@ -5,10 +5,12 @@ use axum::Router;
 use axum::{extract, routing};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
+use tokio::sync::mpsc::{error::SendError, Sender};
 use tokio::sync::Notify;
 
 #[derive(Clone)]
 pub struct AppState {
+    code_sender: Option<Sender<String>>,
     notify: Arc<Notify>,
 }
 
@@ -17,10 +19,37 @@ struct AuthorizedQuery {
     code: String,
 }
 
-impl AppState {
-    pub fn new() -> Self {
+impl Default for AppState {
+    fn default() -> Self {
         let notify = Arc::new(Notify::new());
-        Self { notify }
+        Self {
+            code_sender: None,
+            notify,
+        }
+    }
+}
+
+impl AppState {
+    pub fn new(sender: Sender<String>) -> Self {
+        let notify = Arc::new(Notify::new());
+        Self {
+            code_sender: Some(sender),
+            notify,
+        }
+    }
+
+    pub fn clone_without_sender(&self) -> Self {
+        Self {
+            code_sender: None,
+            notify: self.notify.clone(),
+        }
+    }
+
+    pub async fn send_code(&self, code: String) -> Result<(), SendError<String>> {
+        let Some(sender) = &self.code_sender else {
+            return Ok(());
+        };
+        sender.send(code).await
     }
 
     pub async fn notify_shutdown(&self) {
@@ -59,23 +88,30 @@ async fn shutdown(st: extract::State<AppState>) -> &'static str {
     "shutdown"
 }
 
-#[tracing::instrument()]
-async fn authorized(_params: extract::Query<AuthorizedQuery>) -> &'static str {
+#[tracing::instrument(skip_all)]
+async fn authorized(
+    st: extract::State<AppState>,
+    extract::Query(params): extract::Query<AuthorizedQuery>,
+) -> &'static str {
     tracing::info!("authorized");
+    if let Err(e) = st.0.send_code(params.code).await {
+        tracing::error!("failed to send authorization code {e}");
+    }
     "success!"
 }
 
 pub async fn listen() -> anyhow::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = TcpListener::bind(addr).await?;
-    let state = AppState::new();
+    let state = AppState::default();
+    let signal_state = state.clone_without_sender();
     let app = Router::new()
         .route("/ping", routing::get(ping))
         .route("/shutdown", routing::get(shutdown))
         .route("/_authorized", routing::get(authorized))
-        .with_state(state.clone());
+        .with_state(state);
     axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal(state))
+        .with_graceful_shutdown(shutdown_signal(signal_state))
         .await?;
     Ok(())
 }
