d754d35798bb65dda2a2bf8d385c4b9c3c1dbc9c
H1rono <hronok66@gmail.com>
2024-03-27 18:59:35 +0900

    Split module `client`

---
+++ src/client.rs
@@ -0,0 +1,143 @@
+use std::{borrow::Cow, collections::HashMap};
+
+use anyhow::anyhow;
+use serde_json::Value;
+
+#[derive(Clone)]
+pub struct Client {
+    inner: reqwest::Client,
+    api_base_path: String,
+    client_id: String,
+    access_token: Option<String>,
+}
+
+#[derive(Clone, Default)]
+pub struct ClientBuilder {
+    api_base_path: Option<String>,
+    client_id: Option<String>,
+    access_token: Option<String>,
+}
+
+#[allow(unused)]
+impl ClientBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn build(self) -> Client {
+        let ClientBuilder {
+            api_base_path,
+            client_id,
+            access_token,
+        } = self;
+        let api_base_path = api_base_path.unwrap_or_else(|| "https://q.trap.jp/api/v3".to_string());
+        let client_id = client_id.expect("client_id is not set");
+        Client {
+            inner: reqwest::Client::new(),
+            api_base_path,
+            client_id,
+            access_token,
+        }
+    }
+
+    pub fn api_base_path<'a, S>(self, base_path: S) -> Self
+    where
+        S: Into<Cow<'a, str>>,
+    {
+        let base_path = base_path.into().into_owned();
+        Self {
+            api_base_path: Some(base_path),
+            ..self
+        }
+    }
+
+    pub fn client_id<'a, S>(self, client_id: S) -> Self
+    where
+        S: Into<Cow<'a, str>>,
+    {
+        let client_id = client_id.into().into_owned();
+        Self {
+            client_id: Some(client_id),
+            ..self
+        }
+    }
+
+    pub fn access_token<'a, S>(self, access_token: S) -> Self
+    where
+        S: Into<Cow<'a, str>>,
+    {
+        let access_token = access_token.into().into_owned();
+        Self {
+            access_token: Some(access_token),
+            ..self
+        }
+    }
+}
+
+#[allow(unused)]
+impl Client {
+    pub fn builder() -> ClientBuilder {
+        ClientBuilder::new()
+    }
+
+    pub fn authorize_endpoint(&self) -> String {
+        let Self {
+            api_base_path,
+            client_id,
+            ..
+        } = self;
+        format!("{api_base_path}/oauth2/authorize?response_type=code&client_id={client_id}")
+    }
+
+    #[tracing::instrument(skip(self), fields(%self.client_id))]
+    pub async fn authorize_with(self, code: &str) -> anyhow::Result<Self> {
+        let Self {
+            inner,
+            api_base_path,
+            client_id,
+            ..
+        } = self;
+        let token_request_form: HashMap<&str, &str> = [
+            ("grant_type", "authorization_code"),
+            ("client_id", &client_id),
+            ("code", &code),
+        ]
+        .into_iter()
+        .collect();
+        let token_response: Value = inner
+            .post(format!("{api_base_path}/oauth2/token"))
+            .form(&token_request_form)
+            .send()
+            .await?
+            .json()
+            .await?;
+        tracing::debug!("received token: {token_response}");
+        let access_token = token_response
+            .get("access_token")
+            .and_then(|a| a.as_str())
+            .ok_or_else(|| anyhow!("received unexpected response: {token_response}"))?
+            .to_string();
+        Ok(Self {
+            inner,
+            api_base_path,
+            client_id,
+            access_token: Some(access_token),
+        })
+    }
+
+    pub async fn get_me(&self) -> anyhow::Result<Value> {
+        let Some(access_token) = &self.access_token else {
+            return Err(anyhow!("authorize required before calling API"));
+        };
+        let url = format!("{}/users/me", self.api_base_path);
+        let me: Value = self
+            .inner
+            .get(url)
+            .bearer_auth(access_token)
+            .send()
+            .await?
+            .json()
+            .await?;
+        Ok(me)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::collections::HashMap;
-
-use reqwest::Client;
 use serde_json::Value;
 use tokio::process::Command;
 use tokio::sync::mpsc;
 use tracing_subscriber::EnvFilter;
 
+mod client;
 mod routes;
 
 #[tokio::main]
@@ -16,15 +14,17 @@ async fn main() -> anyhow::Result<()> {
         .init();
     let client_id = std::env::var("CLIENT_ID")?;
     let (code_tx, mut code_rx) = mpsc::channel(2);
+    let client = client::Client::builder()
+        .client_id(client_id)
+        .api_base_path("https://q.trap.jp/api/v3")
+        .build();
 
     let route_state = routes::AppState::new(code_tx);
     let route = tokio::spawn(routes::listen(route_state));
 
-    const OAUTH_ENDPOINT: &str = "https://q.trap.jp/api/v3/oauth2/authorize";
+    // FIXME: macOS only
     Command::new("open")
-        .arg(format!(
-            "{OAUTH_ENDPOINT}?response_type=code&client_id={client_id}"
-        ))
+        .arg(client.authorize_endpoint())
         .output()
         .await?;
 
@@ -32,34 +32,9 @@ async fn main() -> anyhow::Result<()> {
         tracing::error!("channel closed unexpectedly");
         return Ok(());
     };
-    let token_request_form: HashMap<&str, &str> = [
-        ("grant_type", "authorization_code"),
-        ("client_id", &client_id),
-        ("code", &code),
-    ]
-    .into_iter()
-    .collect();
-    let client = Client::new();
-    let token_response: Value = client
-        .post("https://q.trap.jp/api/v3/oauth2/token")
-        .form(&token_request_form)
-        .send()
-        .await?
-        .json()
-        .await?;
-    tracing::debug!("received token: {token_response}");
+    let client = client.authorize_with(&code).await?;
 
-    let Some(token) = token_response.get("access_token").and_then(|a| a.as_str()) else {
-        tracing::error!("requested token was not found in the response {token_response}");
-        return Ok(());
-    };
-    let me: Value = client
-        .get("https://q.trap.jp/api/v3/users/me")
-        .bearer_auth(token)
-        .send()
-        .await?
-        .json()
-        .await?;
+    let me: Value = client.get_me().await?;
     tracing::debug!("your info: {me}");
     let id = me.get("id").and_then(|i| i.as_str());
     let name = me.get("name").and_then(|n| n.as_str());
